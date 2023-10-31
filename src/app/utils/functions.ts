d2860889f20dddad4579b488339883a332d02c3a
Pedro Enrique Pereira Velosa <m_pedro.velosa@fiap.com.br>
2023-10-30 21:39:57 -0300

    helper function random color

---
+++ src/app/utils/functions.ts
@@ -0,0 +1,12 @@
+export const generateRColor = () => {
+  const color =
+    "rgb(" +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
+
+  return color;
+};
